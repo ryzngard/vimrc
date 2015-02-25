44893994f6a3493ccd64c3de974576e5fb41780f
Andrew Hall (METAL) <andrha@microsoft.com>
2015-02-25 11:10:28 -0800

    Update Powershell script

--- install_awesome_vimrc.ps1
+++ install_awesome_vimrc.ps1
@@ -1,13 +1,7 @@
-$basic = cat ~/.vim_runtime/vimrcs/basic.vim
-$filetypes = cat ~/.vim_runtime/vimrcs/filetypes.vim
-$plugins_config = cat ~/.vim_runtime/vimrcs/plugins_config.vim
-$extended = cat ~/.vim_runtime/vimrcs/extended.vim
-$my_configs = ""
+rm ~/.vimrc
+ls ~/.vim_runtime/vimrcs -filter *.vim | cat | Add-Content ~/.vimrc
 
 if (test-path ~/.vim_runtime/my_configs.vim)
 {
-    $my_configs = cat ~/.vim_runtime/my_configs.vim
+    cat ~/.vim_runtime/my_configs.vim | add-content ~/.vimrc
 }
-
-$output = $basic + "`r" + $filetypes + "`r" + $plugins_config + "`r" + $extended + "`r" +  $my_configs 
-$output | out-file ~/.vimrc
